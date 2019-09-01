8c6c134516b1e9ef20c848b52a80569ce3e81dc0
mjtabaya <mjtabaya@gmail.com>
2019-09-01 17:18:37 +0800

    Add first and last names to User

--- db/migrate/20190901091603_add_names_to_users.rb
+++ db/migrate/20190901091603_add_names_to_users.rb
@@ -2,7 +2,7 @@
 
 class AddNamesToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :first_name, :string, null: false
-    add_column :users, :last_name,  :string, null: false
+    add_column :users, :first_name, :string, null: false, default: ''
+    add_column :users, :last_name,  :string, null: false, default: ''
   end
 end
